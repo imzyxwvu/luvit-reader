fee44e15f455ae23f362517676108af964e78d82
Zyxwvu Shi <imzyxwvu@icloud.com>
2015-02-10 01:10:05 +0900

    add package.lua

---
+++ package.lua
@@ -0,0 +1,7 @@
+return {
+	name = "imzyxwvu/reader",
+	version = "0.0.1",
+	description = "Decoder-based Stream Reader for Luvit",
+	author = "zyxwvu Shi <imzyxwvu@icloud.com>",
+	dependencies = {},
+}
